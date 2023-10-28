81feff64c733febc616be30246a86105ae96c245
Taras <tbatyuchok@gmail.com>
2023-10-28 23:19:16 +0300

    add action creation

--- Redux-base/src/App.jsx
+++ Redux-base/src/App.jsx
@@ -1,30 +1,127 @@
-import { useState } from 'react'
-import { useDispatch, useSelector} from "react-redux";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-
-import './App.css'
+import "./App.css";
+import { addCustomerAction, removeCustomerAction } from "./store/customerReducer";
+import { addCashAction, getCashAction } from "./store/cashReducer";
 
 function App() {
   const dispatch = useDispatch();
-  const cash  = useSelector( state => state.cash.cash)
+  const cash = useSelector((state) => state.cash.cash);
+  const customer = useSelector((state) => state.customer.customers);
 
 
+  const addCash = () => {
+    // dispatch({ type: "ADD_CASH", payload: 1 });
+    dispatch(addCashAction(1));
+  };
 
-  const addCash = ()=>{
-      dispatch({type:"ADD_CASH",  payload: 1})
-  }
+  const getCash = () => {
+    // dispatch({ type: "GET_CASH", payload: 1 });
+    dispatch(getCashAction(1));
+  };
 
-  const getCash =()=>{
-    dispatch({type: "GET_CASH", payload: 1})
-  }
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now(),
+    };
+    dispatch(addCustomerAction(customer));
+  };
+
+  const removeCustomer = (customer) => {
+    dispatch(removeCustomerAction(customer.id));
+  };
 
   return (
     <>
-    <p>{cash}</p>
-    <button onClick={() =>addCash()}>Plus</button>
-    <button onClick={() =>getCash()}> Minus</button>
+      <p>{cash}</p>
+      <div style={{ display: "flex", gap: "10px" }}>
+        <button
+          style={{
+            padding: "10px 20px",
+            background: "#4CAF50",
+            border: "none",
+            color: "white",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+          onClick={() => addCash()}
+        >
+          Plus
+        </button>
+        <button
+          style={{
+            padding: "10px 20px",
+            background: "#f44336",
+            border: "none",
+            color: "white",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+          onClick={() => getCash()}
+        >
+          Minus
+        </button>
+        <button
+          style={{
+            padding: "10px 20px",
+            background: "#008CBA",
+            border: "none",
+            color: "white",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+          onClick={() => addCustomer(prompt())}
+        >
+          Add Customer
+        </button>
+      </div>
+
+      {customer.length > 0 ? (
+        customer.map((item) => {
+          return (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                border: "1px solid #ccc",
+                borderRadius: "5px",
+                padding: "10px",
+                margin: "10px",
+              }}
+            >
+              <div>{item.name}</div>
+              <button
+                style={{
+                  border: "1px solid #ccc",
+                  borderRadius: "5px",
+                  padding: "5px 10px",
+                  cursor: "pointer",
+                }}
+                onClick={() => removeCustomer(item)}
+              >
+                Remove
+              </button>
+            </div>
+          );
+        })
+      ) : (
+        <div
+          style={{
+            border: "1px solid #ccc",
+            borderRadius: "5px",
+            padding: "10px",
+            margin: "10px",
+            textAlign: "center",
+          }}
+        >
+          No customer
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Redux-base/src/store/index.js
+++ Redux-base/src/store/index.js
@@ -1,11 +1,12 @@
-import { legacy_createStore as createStore, combineReducers } from "redux";
+import { legacy_createStore as createStore, combineReducers, applyMiddleware } from "redux";
 
 import { customerReducer } from "./customerReducer";
 import { cashReducer } from "./cashReducer";
+import thunk from "redux-thunk"
 
 const rootReducer = combineReducers({
   cash:cashReducer,
   customer:customerReducer,
 })
 
-export const  store = createStore(rootReducer);
+export const  store = createStore(rootReducer, applyMiddleware(thunk));
