07cdea4fceefa71bc456d5597e8ff4fef4df94ea
Taras <tbatyuchok@gmail.com>
2024-01-20 15:37:41 +0200

    install Json-server

--- RTK-Query/src/services/PostServices.ts
+++ RTK-Query/src/services/PostServices.ts
@@ -1,9 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { IPost } from "../models/IPost";
 
+const JsonPlaceholder = 'https://jsonplaceholder.typicode.com/';
+const JsonServer = 'https://jsonplaceholder.typicode.com/';
+
 export const postAPI = createApi({
   reducerPath: "postAPI",
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: JsonServer }),
   endpoints: (build) => ({
       fetchAllPosts: build.query<IPost[], number>({
         query: (limit: number = 5)=>({
