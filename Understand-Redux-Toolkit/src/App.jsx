c98b8bc315b733385cdfa16b951e7af6b1e44abd
Taras <tbatyuchok@gmail.com>
2024-04-19 23:45:40 +0300

    add new starter for RTK

---
+++ Understand-Redux-Toolkit/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div className="w-screen h-screen  flex-col dark:bg-slate-400 p-[20px] gap-2">
+
+        <h1>Hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
